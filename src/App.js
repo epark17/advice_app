0f5304e7efda8073da6897d1203bec6146ac08bf
Esther Park <estherspark91@gmail.com>
2020-09-24 19:53:01 -0400

    Fetched advice from API and rendering in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.module.css';
+
+class App extends React.Component {
+  state = { advice: '' };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  // Using https://api.adviceslip.com/
+  /*
+  Example of returned JSON:
+  {
+    slip: {
+      id: 51,
+      advice: "It's wrong to be right."
+    }
+  }
+  */
+  fetchAdvice = async () => {
+    try {
+      // Grabbing just the 'advice' by deconstructing JSON
+      /* ONE WAY
+      const {
+        data: {
+          slip: { advice },
+        },
+      } = await axios.get('https://api.adviceslip.com/advice');
+      */
+
+      const res = await axios.get('https://api.adviceslip.com/advice');
+      const { advice } = res.data.slip;
+      // console.log(advice);
+      this.setState({ advice });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    const { advice } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{advice}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
